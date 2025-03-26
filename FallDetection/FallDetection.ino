732f86886b7d5dd1c7f6a1f542a8cbd7256ef2c5
levandung-it03 <levandung.it03@gmail.com>
2025-03-26 14:31:05 +0700

    Update Arduino Code

--- FallDetection/FallDetection.ino
+++ FallDetection/FallDetection.ino
@@ -36,7 +36,7 @@ QueueHandle_t mpuQueue;
 
 // Queue Configuration
 #define MAX_BUFFER_SAMPLES 60
-#define PREDICT_WINDOW 30
+#define PREDICT_WINDOW 20
 #define SENSOR_DELAY 100  // 100ms per sample
 #define ACC_THRESHOLD 1.5
 
@@ -61,56 +61,59 @@ void runInference(float input_data[PREDICT_WINDOW][6]) {
   }
 }
 
-void predictWithModel() {
-  if (uxQueueMessagesWaiting(mpuQueue) < PREDICT_WINDOW) return;
-
+void predictWithModel(void* pvParameters) {
   float input_data[PREDICT_WINDOW][6];
-
-  // Retrieve first 30 samples and remove them from buffer
-  for (int i = 0; i < PREDICT_WINDOW; i++) {
-    if (xQueueReceive(mpuQueue, &input_data[i], 0) == pdFALSE) {
-      Serial.println("Queue underflow, insufficient data!");
-      return;
+  while (1) {
+    if (uxQueueMessagesWaiting(mpuQueue) < PREDICT_WINDOW) {
+      vTaskDelay(pdMS_TO_TICKS(SENSOR_DELAY));
+      continue;
     }
-  }
 
-  Serial.println("Running TensorFlow Lite Model...");
-  runInference(input_data);
+    // Retrieve first 30 samples and remove them from buffer
+    for (int i = 0; i < PREDICT_WINDOW; i++) {
+      if (xQueueReceive(mpuQueue, &input_data[i], 0) == pdFALSE) {
+        Serial.println("Queue underflow, insufficient data!");
+        return;
+      }
+    }
+
+    Serial.println("Running TensorFlow Lite Model...");
+    runInference(input_data);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
 }
 
 void readMPU6050Task(void* pvParameters) {
   float sample[6];
+  bool isNotReceiving[1];
 
   while (1) {
     // Read MPU6050 values
     mpu6050.update();
 
-    // Save as Buffer[[accX, accY, accZ, gyroX, gyroY, gyroZ],...]
-    // Instead of required predicted datatype of model: [[[accX], [accY], [accZ], [gyroX], [gyroY], [gyroZ]]] - each line.
-    // To meet the memory size demand
-    if (abs(mpu6050.getAccX()) <= ACC_THRESHOLD && abs(mpu6050.getAccY()) <= ACC_THRESHOLD && abs(mpu6050.getAccZ()) <= ACC_THRESHOLD
-      && uxQueueMessagesWaiting(mpuQueue) == 0) {
+    isNotReceiving[0] = abs(mpu6050.getAccX()) <= ACC_THRESHOLD
+      && abs(mpu6050.getAccY()) <= ACC_THRESHOLD
+      && abs(mpu6050.getAccZ()) <= ACC_THRESHOLD
+      && uxQueueMessagesWaiting(mpuQueue) == 0;
+
+    if (isNotReceiving[0]) {
       vTaskDelay(pdMS_TO_TICKS(SENSOR_DELAY));
       continue;
+    } else {
+      // Save as Buffer[[accX, accY, accZ, gyroX, gyroY, gyroZ],...]
+      // Instead of required predicted datatype of model: [[[accX], [accY], [accZ], [gyroX], [gyroY], [gyroZ]]] - each line.
+      // To meet the memory size demand
+      sample[0] = mpu6050.getAccX();
+      sample[1] = mpu6050.getAccY();
+      sample[2] = mpu6050.getAccZ();
+      sample[3] = mpu6050.getGyroX();
+      sample[4] = mpu6050.getGyroY();
+      sample[5] = mpu6050.getGyroZ();
+      // Add sample to queue
+      if (xQueueSend(mpuQueue, &sample, portMAX_DELAY) != pdPASS) {
+        Serial.println("Queue is full, dropping data!");
+      }
     }
-
-    sample[0] = mpu6050.getAccX();
-    sample[1] = mpu6050.getAccY();
-    sample[2] = mpu6050.getAccZ();
-    sample[3] = mpu6050.getGyroX();
-    sample[4] = mpu6050.getGyroY();
-    sample[5] = mpu6050.getGyroZ();
-
-    // Add sample to queue
-    if (xQueueSend(mpuQueue, &sample, portMAX_DELAY) != pdPASS) {
-      Serial.println("Queue is full, dropping data!");
-    }
-
-    // Check if we have enough data for prediction
-    if (uxQueueMessagesWaiting(mpuQueue) >= PREDICT_WINDOW) {
-      predictWithModel();
-    }
-
     vTaskDelay(pdMS_TO_TICKS(SENSOR_DELAY));  // Delay 100ms
   }
 }
@@ -123,8 +126,7 @@ void setup() {
   tflModel = tflite::GetModel(gesture_model);
   if (tflModel->version() != TFLITE_SCHEMA_VERSION) {
     Serial.println("Model schema mismatch!");
-    while (1)
-      ;
+    while (1);
   }
 
   // Create an interpreter
@@ -140,8 +142,9 @@ void setup() {
   // Create queue for MPU data (buffer holds max 60 samples)
   mpuQueue = xQueueCreate(MAX_BUFFER_SAMPLES, sizeof(float[6]));
 
-  // Start MPU6050 task
+  // Start tasks
   xTaskCreatePinnedToCore(readMPU6050Task, "MPU6050 Task", 4096, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(predictWithModel, "Model Predection", 4096, NULL, 1, NULL, 1);
 }
 
 void loop() {
